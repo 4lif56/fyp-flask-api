9c0a4d14ea71bb593b37ffaf5b97f4c0dc8e092c
Ken Takakura <muhd.alif13@gmail.com>
2025-10-18 15:41:47 +0800

    Added Flask-CORS support

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import pandas as pd
 from sklearn.ensemble import IsolationForest
 import io
 
 app = Flask(__name__)
-from flask_cors import CORS
 CORS(app)
 
 
